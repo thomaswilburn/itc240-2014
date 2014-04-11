6b2aecd5be9e2075d6078b7fa29ef127e5667789
Thomas Wilburn <news@thomaswilburn.net>
2014-04-10 18:32:13 -0700

    Added first class demonstration files

---
+++ in-class/040814/index.php
@@ -0,0 +1,12 @@
+<?php
+$page_title = "Index page!";
+include("head.inc");
+?>
+
+This space intentionally left NOT blank.
+
+<?php
+//this shows debug information on the page
+//phpinfo();
+?>
+This is also blank.
---
+++ in-class/040814/second.php
@@ -0,0 +1,6 @@
+<?php
+$page_title = "Second page!";
+include("head.inc");
+?>
+
+This is the second page.
